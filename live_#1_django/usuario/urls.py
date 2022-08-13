bb4a2d7e43700e65f24d777c3d8bb6ab91efa22d
Arend <bi_arend@hotmail.com>
2022-08-13 17:22:51 -0300

    Inserir no banco de dados

--- live_#1_django/usuario/urls.py
+++ live_#1_django/usuario/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path('cadastro/', views.cadastro, name = "cadastro"),
     path('login/', views.login, name= "login"),
-    path('', views.home, name = "home")
+    path('', views.home, name = "home"),
+    path('salvar/', views.salvar, name= "salvar")
 
 ]
--- live_#1_django/usuario/views.py
+++ live_#1_django/usuario/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from .models import Usuario
 
 def cadastro(request):
     
@@ -13,6 +14,26 @@ def login(request):
     sobrenome = request.GET.get('sobrenome')
     return render(request, 'login/login.html', {'nome_usuario': nome, 'sobrenome_usuario': sobrenome})
 
+
 def home(request):
 
     return HttpResponse('Você está na home do usuário')
+
+
+def salvar(request):
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    idade = request.POST.get('idade')
+
+    usuario = Usuario(
+
+        nome = nome,
+        sobrenome = sobrenome,
+        idade = idade
+
+    )
+
+    usuario.save()
+
+    return HttpResponse(f"{nome} {sobrenome} {idade}")
